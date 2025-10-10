6861104c901ff23f112124b2eccd9622717f44a1
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-10-10 12:50:00 +0000

    feat: 02.6 - dates

--- Ch02/02_06b/main.go
+++ Ch02/02_06b/main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 
-	fmt.Println("Dates and times")
+	t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+	fmt.Printf("Go launched at %s\n", t)
 
+	now := time.Now()
+	fmt.Printf("The time currently is %s\n", now)
+	fmt.Printf("This object's type is %T\n", now)
+
+	fmt.Printf(now.Format(time.ANSIC) + "\n")
+
+	tomorrow := now.AddDate(0, 0, 1)
+	fmt.Printf(tomorrow.Format(time.ANSIC) + "\n")
+
+	format := "Mon 2006-02-01"
+	fmt.Printf(tomorrow.Format(format) + "\n")
 }
