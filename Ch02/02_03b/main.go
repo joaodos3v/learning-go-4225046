e67c0edfffda7bb3ca4ee081ae6633fa5a9900f8
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-10-10 12:21:37 +0000

    feat: 02.3 - inputs from the user for a CLI app

--- Ch02/02_03b/main.go
+++ Ch02/02_03b/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print(("Enter text: "))
+	str, _ := reader.ReadString('\n') // _ is used to ignore the error
+	fmt.Println(str)
+
+	fmt.Print(("Enter a number: "))
+	str, _ = reader.ReadString('\n')                         // We should only use := when we're declaring the variables for the first time
+	f, err := strconv.ParseFloat(strings.TrimSpace(str), 64) // 64 is the precision
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("The number:", f)
+	}
 }
