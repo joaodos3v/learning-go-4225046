055de0601806653f7c5c0f901a52ad2d09230ee5
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-10-13 12:38:29 +0000

    feat: 03.5 - maps to store unordered data

--- Ch03/03_05b/main.go
+++ Ch03/03_05b/main.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	fmt.Println("Maps")
+	states := make(map[string]string)
+
+	states["WA"] = "Washington"
+	states["OR"] = "Oregon"
+	states["CA"] = "California"
+
+	fmt.Println(states)
+
+	california := states["CA"]
+	fmt.Println(california)
+
+	delete(states, "OR")
+	fmt.Println(states)
+
+	states["NY"] = "New York"
+	fmt.Println(states)
+
+	for key, value := range states {
+		fmt.Printf("%v: %v\n", key, value)
+	}
+
+	keys := make([]string, len(states))
+	i := 0
+	for key := range states {
+		keys[i] = key
+		i++
+	}
+	sort.Strings(keys)
+	fmt.Println("\nSorted")
+
+	for i := range keys {
+		fmt.Println((states[keys[i]]))
+	}
 }
